9e345a09ede5ae983028dd9d3711cb85d0dde9cf
Angélique <angeliquelebas2008@gmail.com>
2022-02-16 16:17:40 +0100

    [guess_the_number] Finish the game

--- Simple-Cpp-Setup-main/src/main.cpp
+++ Simple-Cpp-Setup-main/src/main.cpp
@@ -20,9 +20,32 @@ int get_int_from_user()
     return n;
 }
 
+void play_guess_the_number()
+{
+    static constexpr int MIN            = 0;
+    static constexpr int MAX            = 100;
+    const int            program_number = rand(MIN, MAX);
+    std::cout << "I pick a number between " << MIN << " and " << MAX << " :" << '\n';
+
+    bool finished = false;
+
+    while (!finished) {
+        const int player_number = get_int_from_user();
+
+        if (player_number == program_number) {
+            finished = true;
+            std::cout << "Congrats, you won!" << '\n';
+        }
+        else if (player_number < program_number) {
+            std::cout << "Greater " << '\n';
+        }
+        else {
+            std::cout << "Smaller " << '\n';
+        }
+    }
+}
+
 int main()
 {
-    std::cout << "Choose a number: " << '\n';
-    const int number = get_int_from_user();
-    std::cout << "Your number is " << number << "." << '\n';
+    play_guess_the_number();
 }
