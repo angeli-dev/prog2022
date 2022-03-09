d1ac2f5a3cb5f86da5e5a0795f00d15f761ccbe9
Angélique <angeliquelebas2008@gmail.com>
2022-03-09 16:46:44 +0100

    [hangman] Debug hangman

--- src/hangman.cpp
+++ src/hangman.cpp
@@ -1,6 +1,6 @@
 #include <array>
 #include <iostream>
-#include <string_view>
+//#include <string_view>
 #include "algorithm"
 #include "get_input_from_user.h"
 #include "random.h"
@@ -13,16 +13,16 @@ std::string pick_a_word_to_guess()
         "imac",
         "opengl",
     };
-    const int n = rand<size_t>(0, words.size() - 1);
+    const size_t n = rand<size_t>(0, words.size() - 1);
     return words[n];
 }
 
-void show_number_of_lives(int number_of_lives)
+void show_number_of_lives(const int number_of_lives)
 {
-    std::cout << number_of_lives << "/n";
+    std::cout << "Nombre de vie : " << number_of_lives << std::endl;
 }
 
-bool player_is_alive(int number_of_lives)
+bool player_is_alive(const int number_of_lives)
 {
     if (number_of_lives <= 0) {
         return 0;
@@ -42,15 +42,16 @@ void show_word_to_guess_with_missing_letters(const std::string& word, const std:
 {
     for (size_t letter = 0; letter < word.size(); letter++) {
         if (letters_guessed[letter]) {
-            std::cout << letter;
+            std::cout << word[letter];
         }
         else {
             std::cout << "_";
         }
     }
+    std::cout << '\n';
 }
 
-bool word_contains(char letter, std::string word)
+bool word_contains(const std::string letter, const std::string word)
 {
     if (word.find(letter) == std::string::npos) {
         return 0;
@@ -58,12 +59,14 @@ bool word_contains(char letter, std::string word)
     return 1;
 }
 
-void mark_as_guessed(char guessed_letter, std::vector<bool>& letters_guessed, std::string word_to_guess)
+void mark_as_guessed(const std::string guessed_letter, std::vector<bool>& letters_guessed, const std::string word_to_guess)
 {
     size_t pos = 0;
+    std::cout << word_to_guess.find(guessed_letter, pos) << std::endl;
     while (word_to_guess.find(guessed_letter, pos) != std::string::npos) {
         pos                  = word_to_guess.find(guessed_letter, pos);
         letters_guessed[pos] = 1;
+        pos++;
     }
 }
 
@@ -72,7 +75,7 @@ void remove_one_life(int& lives_count)
     lives_count--;
 }
 
-void show_congrats_message(std::string word_to_guess)
+void show_congrats_message(const std::string word_to_guess)
 {
     std::cout << "Congratulations!" << std::endl;
     std::cout << "The word was: " << word_to_guess << "!" << std::endl;
@@ -86,12 +89,13 @@ void show_defeat_message()
 void play_hangman()
 {
     int               number_of_lives = 10;
-    std::string       word            = pick_a_word_to_guess();
+    const std::string word            = pick_a_word_to_guess();
     std::vector<bool> letters_guessed(word.size(), 0);
+
     while (player_is_alive(number_of_lives) && !player_has_won(letters_guessed)) {
         show_number_of_lives(number_of_lives);
         show_word_to_guess_with_missing_letters(word, letters_guessed);
-        const auto guess = get_input_from_user<char>();
+        const auto guess = get_input_from_user<std::string>();
         if (word_contains(guess, word)) {
             mark_as_guessed(guess, letters_guessed, word);
         }
