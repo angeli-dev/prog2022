391836f489fa7a6b277a8047700815539fb20818
Angélique <angeliquelebas2008@gmail.com>
2022-04-20 17:45:21 +0200

    [noughts_and_crosses] Draw a black window

--- src/noughts_and_crosses.cpp
+++ src/noughts_and_crosses.cpp
@@ -1,3 +1,17 @@
 #include "noughts_and_crosses.h"
+#include <p6/p6.h>
 
-void play_noughts_and_crosses() {}
+void draw_circle(p6::Context ctx)
+{
+    ctx.circle(p6::Center{ctx.mouse()},
+               p6::Radius{0.3f});
+}
+
+void play_noughts_and_crosses()
+{
+    auto ctx   = p6::Context{{720, 720, "Hello p6"}};
+    ctx.update = [&]() {
+        ctx.background({0.f, 0.f, 0.f});
+    };
+    ctx.start();
+}
