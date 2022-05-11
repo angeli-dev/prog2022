f53437b339b397498ba551dc0024919bb6ff46be
Angélique <angeliquelebas2008@gmail.com>
2022-05-11 19:56:32 +0200

    [noughts_and_crosses] Create the grid

--- src/noughts_and_crosses.cpp
+++ src/noughts_and_crosses.cpp
@@ -1,17 +1,17 @@
 #include "noughts_and_crosses.h"
 #include <p6/p6.h>
 
-void draw_circle(p6::Context ctx)
-{
-    ctx.circle(p6::Center{ctx.mouse()},
-               p6::Radius{0.3f});
-}
-
 void play_noughts_and_crosses()
 {
     auto ctx   = p6::Context{{720, 720, "Hello p6"}};
     ctx.update = [&]() {
         ctx.background({0.f, 0.f, 0.f});
+        ctx.stroke        = p6::Color{1.f, 1.f, 1.f, 0.75f};
+        ctx.stroke_weight = 0.01f;
+        ctx.line(glm::vec2{float(-2) / 6, 1}, glm::vec2{float(-2) / 6, -1});
+        ctx.line(glm::vec2{float(2) / 6, 1}, glm::vec2{float(2) / 6, -1});
+        ctx.line(glm::vec2{1, float(-2) / 6}, glm::vec2{-1, float(-2) / 6});
+        ctx.line(glm::vec2{1, float(2) / 6}, glm::vec2{-1, float(2) / 6});
     };
     ctx.start();
 }
